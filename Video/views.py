d12201791478b9992d14ebac7d3b51fa5a5e824d
sNishant011 <49269819+sNishant011@users.noreply.github.com>
2020-09-09 10:51:54 +0545

    Update views.py

--- Video/views.py
+++ Video/views.py
@@ -1,1 +1,14 @@
+from django.shortcuts import render
+from .models import NewVideo, VideoExplanation, MoreVideo
+from Home.models import About
 
+
+def index(request):
+    videos = NewVideo.objects.all()
+    abouts = About.objects.all()
+    more_videos = MoreVideo.objects.all()
+    explanations = VideoExplanation.objects.all()
+    return render(request, 'videos.html', {'videos': videos,
+                                           'abouts': abouts,
+                                           'explanations': explanations,
+                                           'more_videos': more_videos})
